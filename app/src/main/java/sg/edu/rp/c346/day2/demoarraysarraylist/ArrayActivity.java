53187b1145c2488db5134e74f693b6145a14f2ec
19013886 <19013886@myrp.edu.sg>
2021-06-08 10:39:25 +0800

    P07 - Recap on Array and ArrayList Done

--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
@@ -8,17 +8,30 @@ import android.widget.TextView;
 public class ArrayActivity extends AppCompatActivity {
 
     TextView tv;
+    String[] fruits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_array);
 
+        //continue view the code in the worksheet
+        this.setTitle("Fruits");
+        fruits = new String[3];
+        fruits[0] = "apple";
+        fruits[1] = "banana";
+        fruits[2] = "cherry";
+
         tv = findViewById(R.id.textView1);
 
-        //continue view the code in the worksheet
+        String text = "Fruits\n";
+        text += "=====\n";
 
+        for (int i = 0; i<fruits.length; i++) {
+            text += fruits[i] + "\n";
+        }
 
+        tv.setText(text);
 
     }
 }
